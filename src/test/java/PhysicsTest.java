0865b23e1626f6cd337d34dbeb790f1dee56aa71
ethanmcaleavy <ethan.mcaleavy@gmail.com>
2024-04-30 22:48:31 -0600

    added orbital

--- src/test/java/PhysicsTest.java
+++ src/test/java/PhysicsTest.java
@@ -234,6 +234,238 @@ public class PhysicsTest
     }
 
     @Test
+    public void testVolume()
+    {
+        int height = 85;
+        int width = 142;
+
+        Object one = Object.newBuilder()
+                .radius(1)
+                .mass(50)
+                .velocity(new Velocity(3,3))
+                .location(new Location(10,-10))
+                .color(Color.RED)
+                .create();
+
+        Object two = Object.newBuilder()
+                .radius(2)
+                .mass(20)
+                .velocity(new Velocity(4,5))
+                .location(new Location(20,-20))
+                .color(Color.blue)
+                .create();
+
+        Object three = Object.newBuilder()
+                .radius(3)
+                .mass(35)
+                .velocity(new Velocity(5,6))
+                .location(new Location(30,-30))
+                .color(Color.yellow)
+                .create();
+
+        Object four = Object.newBuilder()
+                .radius(4)
+                .mass(60)
+                .velocity(new Velocity(6,7))
+                .location(new Location(40,-40))
+                .color(Color.green)
+                .create();
+
+        Object five = Object.newBuilder()
+                .radius(5)
+                .mass(65)
+                .velocity(new Velocity(25,8))
+                .location(new Location(50,-50))
+                .color(Color.GRAY)
+                .create();
+
+        Object six = Object.newBuilder()
+                .radius(6)
+                .mass(7)
+                .velocity(new Velocity(8,9))
+                .location(new Location(10,-40))
+                .color(Color.PINK)
+                .create();
+
+        Object seven = Object.newBuilder()
+                .radius(7)
+                .mass(8)
+                .velocity(new Velocity(9,-40))
+                .location(new Location(15,-75))
+                .color(Color.darkGray)
+                .create();
+
+        Object eight = Object.newBuilder()
+                .radius(8)
+                .mass(8)
+                .velocity(new Velocity(11,12))
+                .location(new Location(80,-75))
+                .color(Color.magenta)
+                .create();
+
+        Object nine = Object.newBuilder()
+                .radius(9)
+                .mass(15)
+                .velocity(new Velocity(1,-1))
+                .location(new Location(120,-70))
+                .color(Color.orange)
+                .create();
+
+
+        Sim runSim = Sim.newBuilder()
+                .createAndAddMoon(height, width,1)
+                .addObjects(one)
+                .addObjects(two)
+                .addObjects(three)
+                .addObjects(four)
+                .addObjects(five)
+                .addObjects(six)
+//                .addObjects(seven)
+//                .addObjects(eight)
+//                .addObjects(nine)
+                .setTimeSteps(0.0001)
+                .setDuration(70)
+                .showVelocityInGUI(false)
+                .setElastic(true)
+                .setOrbital(false)
+                .run()
+                .executeGUI();
+
+//        assert(runSim.collisionDetected() == true);
+    }
+
+    @Test
+    public void InelasticTest()
+    {
+        int height = 75;
+        int width = 125;
+
+        double mass1 = 200; // kg
+        Location location1 = new Location(14, -30); // meters
+        Velocity velocity1 = new Velocity(8, -12);
+
+        double mass2 = 1; // kg
+        Location location2 = new Location(27, -10);
+        Velocity velocity2 = new Velocity(-3, 7);
+
+        double mass3 = 1; // kg
+        Location location3 = new Location(95, -50);
+        Velocity velocity3 = new Velocity(-20, 9);
+
+        double mass4 = 1; // kg
+        Location location4 = new Location(80, -20);
+        Velocity velocity4 = new Velocity(-10, 4);
+
+        double mass5 = 1; // kg
+        Location location5 = new Location(113, -20);
+        Velocity velocity5 = new Velocity(-10, 4);
+
+        Object objOne = Object.newBuilder()
+                .radius(10)
+                .mass(mass1)
+                .velocity(velocity1)
+                .location(location1)
+                .color(Color.RED)
+                .create();
+
+        Object objTwo = Object.newBuilder()
+                .radius(1)
+                .mass(mass2)
+                .velocity(velocity2)
+                .location(location2)
+                .color(Color.BLUE)
+                .create();
+
+
+        Object objThree = Object.newBuilder()
+                .radius(1)
+                .mass(mass3)
+                .velocity(velocity3)
+                .location(location3)
+                .color(Color.green)
+                .create();
+
+        Object objFour = Object.newBuilder()
+                .radius(1)
+                .mass(mass4)
+                .velocity(velocity4)
+                .location(location4)
+                .color(Color.pink)
+                .create();
+
+        Object objFive = Object.newBuilder()
+                .radius(1)
+                .mass(mass5)
+                .velocity(velocity5)
+                .location(location5)
+                .color(Color.orange)
+                .create();
+
+
+        Sim runSim = Sim.newBuilder()
+                .createAndAddMoon(height, width,1)
+                .addObjects(objOne)
+                .addObjects(objTwo)
+                .addObjects(objThree)
+                .addObjects(objFour)
+                .addObjects(objFive)
+                .setTimeSteps(0.0001)
+                .setDuration(50)
+                .showVelocityInGUI(false)
+                .setElastic(true)
+                .run()
+                .executeGUI();
+
+//        assert(runSim.collisionDetected() == true);
+    }
+
+    @Test
+    public void Orbit()
+    {
+        int height = 85;
+        int width = 145;
+
+        double mass1 = 800; // kg
+        Location location1 = new Location(85, -10); // meters
+        Velocity velocity1 = new Velocity(3, -10);
+
+        double mass2 = 400; // kg
+        Location location2 = new Location(80, -77);
+        Velocity velocity2 = new Velocity(-1, 10);
+
+        Object objOne = Object.newBuilder()
+                .radius(2)
+                .mass(mass1)
+                .velocity(velocity1)
+                .location(location1)
+                .color(Color.RED)
+                .create();
+
+        Object objTwo = Object.newBuilder()
+                .radius(1)
+                .mass(mass2)
+                .velocity(velocity2)
+                .location(location2)
+                .color(Color.BLUE)
+                .create();
+
+        Sim runSim = Sim.newBuilder()
+                .createAndAddMoon(height, width,1)
+                .addObjects(objOne)
+                .addObjects(objTwo)
+                .setTimeSteps(0.0001)
+                .setDuration(50)
+                .showVelocityInGUI(false)
+                .setElastic(true)
+                .setOrbital(true)
+                .run()
+                .executeGUI();
+
+//        assert(runSim.collisionDetected() == true);
+    }
+
+
+    @Test
     public void TestEarthGravity()
     {
         int height = 60;
